162ef996774d184a536635ddc75117f21dcd7381
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2019-07-22 22:05:28 +0200

    fix(build): switched typedocs to custom build to allow typescript@3.5.x

--- src/use-formstate.ts
+++ src/use-formstate.ts
@@ -1,4 +1,4 @@
-import { createInitialState, fromEntries } from './utils'
+import { createInitialState, fromEntries } from './utils';
 import {
   Errors,
   FieldState,
@@ -9,49 +9,50 @@ import {
   SubmitHandler,
   Validation,
   Values
-} from './domain'
-import { useImmer } from 'use-immer'
-import { useCallback, useMemo, useState } from 'react'
+} from './domain';
+import { useImmer } from 'use-immer';
+import { useCallback, useMemo, useState } from 'react';
+export * from './domain';
 
 export default function useFormstate<S extends { [key: string]: any }>(validation: Validation<S>) {
-  const keys: Array<Keyof<S>> = Object.keys(validation) as Array<Keyof<S>>
+  const keys: Array<Keyof<S>> = Object.keys(validation) as Array<Keyof<S>>;
   return (initialValues: InitialValues<S>): Formstate<S> => {
-    const [submitting, setSubmitting] = useState(false)
-    const [state, updateState] = useImmer(createInitialState(keys, validation, initialValues))
-    const valid = Object.values(state.fields).every(field => !field.error)
-    const [beenValidSinceSubmit, setBeenValidSinceSubmit] = useState(valid)
+    const [submitting, setSubmitting] = useState(false);
+    const [state, updateState] = useImmer(createInitialState(keys, validation, initialValues));
+    const valid = Object.values(state.fields).every(field => !field.error);
+    const [beenValidSinceSubmit, setBeenValidSinceSubmit] = useState(valid);
 
     const createFormstate: () => Formstate<S> = () => {
       const values: Values<S> = useMemo(
         () => fromEntries(Object.entries(state.fields).map(([key, field]) => [key, field.value])),
         [state.fields]
-      )
+      );
 
       const onChange = useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => {
           updateState(draft => {
-            const value = event.target.value
-            const oldValue = draft.fields[event.target.name].value
+            const value = event.target.value;
+            const oldValue = draft.fields[event.target.name].value;
 
-            draft.fields[event.target.name].pristine = oldValue === value
-            draft.fields[event.target.name].value = value
+            draft.fields[event.target.name].pristine = oldValue === value;
+            draft.fields[event.target.name].value = value;
             Object.keys(draft.fields).map(key => {
-              draft.fields[key].error = validation[key](draft.fields[key].value, values)
-            })
-          })
+              draft.fields[key].error = validation[key](draft.fields[key].value, values);
+            });
+          });
         },
         [updateState, validation, values]
-      )
+      );
 
       const onBlur = useCallback(
         (event: React.FocusEvent<HTMLInputElement>) => {
           updateState(draft => {
-            const name = event.target.name
-            draft.fields[name].touched = true
-          })
+            const name = event.target.name;
+            draft.fields[name].touched = true;
+          });
         },
         [updateState]
-      )
+      );
 
       const fieldsArray: Array<[Keyof<S>, FieldState]> = Object.entries(state.fields).map(
         ([key, field]) => {
@@ -65,30 +66,30 @@ export default function useFormstate<S extends { [key: string]: any }>(validatio
               onChange,
               onBlur
             }
-          }
-          return [key, fieldstate]
+          };
+          return [key, fieldstate];
         }
-      )
+      );
 
       const errorsArray: Array<[Keyof<S>, string]> = fieldsArray
         .filter(([key, field]) => field.error)
-        .map(([key, field]) => [key, field.error!])
-      const valid = errorsArray.length === 0
+        .map(([key, field]) => [key, field.error!]);
+      const valid = errorsArray.length === 0;
       if (valid) {
-        setBeenValidSinceSubmit(true)
+        setBeenValidSinceSubmit(true);
       }
-      const pristine = fieldsArray.every(([key, field]) => field.pristine)
-      const errors: Errors<S> = fromEntries(errorsArray)
-      const fields: Mapped<S, FieldState> = fromEntries(fieldsArray)
+      const pristine = fieldsArray.every(([key, field]) => field.pristine);
+      const errors: Errors<S> = fromEntries(errorsArray);
+      const fields: Mapped<S, FieldState> = fromEntries(fieldsArray);
       const onSubmit = (fn: SubmitHandler<S>) => (event: React.FormEvent) => {
-        event.preventDefault()
+        event.preventDefault();
         if (errorsArray.length === 0) {
-          setSubmitting(true)
-          fn(values).then(() => setSubmitting(false), () => setSubmitting(false))
+          setSubmitting(true);
+          fn(values).then(() => setSubmitting(false), () => setSubmitting(false));
         } else {
-          setBeenValidSinceSubmit(false)
+          setBeenValidSinceSubmit(false);
         }
-      }
+      };
 
       return {
         submitting,
@@ -98,9 +99,9 @@ export default function useFormstate<S extends { [key: string]: any }>(validatio
         errors,
         fields,
         onSubmit
-      }
-    }
+      };
+    };
 
-    return createFormstate()
-  }
+    return createFormstate();
+  };
 }
--- test/use-formstate.test.ts
+++ test/use-formstate.test.ts
@@ -1,14 +1,5 @@
-import DummyClass from '../src/use-formstate'
-
-/**
- * Dummy test
- */
 describe('Dummy test', () => {
   it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
-
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+    expect(true).toBeTruthy();
+  });
+});
